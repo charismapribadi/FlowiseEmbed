14a6dfcbaa8f9e8a21bbdff7086accb695ebe93b
Charisma Pribadi <charismapribadi@gmail.com>
2024-01-31 18:21:58 +0700

    remove branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      ChatEmbed
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://github.com/charismapribadi/FlowiseEmbed'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Github Repo</span>
       </a>
     </span>
   );
